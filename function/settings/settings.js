7b09dabcf39cc80b4360f0b98d37950bf7a4c33e
ayoubsuk08-jpg <ayoubsuk08@gmail.com>
2026-02-23 00:55:16 +0000

    Update bot and owner phone numbers

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '212674977850',
+ pairingNumber : '212674977850',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
+ nomorwa : '212674977850',
  nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomerown : '212674977850',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
